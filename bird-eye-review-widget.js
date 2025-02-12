0607d34128e477447b0e21f8a9cd0bc38b550ab4
Christopher Zaman <zamanchristopher@gmail.com>
2025-02-12 18:47:48 -0500

    update bird eye review widget

---
+++ bird-eye-review-widget.js
@@ -0,0 +1,3 @@
+document.write(`
+    <script type="text/javascript" src="https://birdeye.com/embed/v7/173800197782921/11/987654321301115183" async></script><div id="bf-revz-widget-987654321301115183" ></div>
+  `);
